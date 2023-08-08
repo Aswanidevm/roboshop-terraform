dd54cb61977c97bc787fd08a3bd367be0171b4e1
Aswanidevm <maswanidev@gmail.com>
2023-08-08 12:11:46 +0530

    scf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-module test {
+module ec2 {
     for_each = var.component
     source = "git::https://github.com/Aswanidevm/tf-module-app.git"
     env = var.env
     component = each.key
+    instance_type = lookup(each.value, "instance_type", "t2.micro" )
 } 
