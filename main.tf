0302093db785dcd94c369561d5796989090a7d11
Aswanidevm <maswanidev@gmail.com>
2023-08-08 21:25:21 +0530

    ih

--- main.tf
+++ main.tf
@@ -3,5 +3,5 @@ module ec2 {
     source = "git::https://github.com/Aswanidevm/tf-module-app.git"
     env = var.env
     component = each.key
-    instance_type = lookup(each.value, "instance_type", "t2.micro" )
+    
 } 
