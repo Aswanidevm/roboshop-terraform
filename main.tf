a59f426a8cddcd80d7119277f7b17d05dc6d24d4
Aswanidevm <maswanidev@gmail.com>
2023-08-16 20:01:48 +0530

    vpc

--- main.tf
+++ main.tf
@@ -1,7 +1,19 @@
+# module ec2 {
+#     for_each = var.component
+#     source = "git::https://github.com/Aswanidevm/tf-module-app.git"
+#     env = var.env
+#     component = each.key
+#     tags = merge(each.value["tags"], var.tags)
+# } 
+
+
+
+
 module ec2 {
     for_each = var.component
-    source = "git::https://github.com/Aswanidevm/tf-module-app.git"
+    source = "git::https://github.com/Aswanidevm/tf-module-vpn.git"
     env = var.env
     component = each.key
     tags = merge(each.value["tags"], var.tags)
 } 
+
