24f2ecdced5ed852b7f756e901c3b8e62ab0abdb
Aswanidevm <maswanidev@gmail.com>
2023-08-17 08:15:34 +0530

    a

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -77,5 +77,6 @@ tags= {
 vpc ={
     main = {
         cidr_block = "10.0.0.0/16"
+        web_subnet_cidr_block = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
     }
 }
--- main.tf
+++ main.tf
@@ -13,8 +13,19 @@ module "vpc" {
    
     source = "git::https://github.com/Aswanidevm/tf-module-vpc.git"
      for_each = var.vpc
-    # env = var.env
+   
     cidr_block = each.value["cidr_block"]
-    # tags = merge(each.value["tags"], var.tags)
+    
+    env = var.env
+
+    azs             = ["eu-east-1a", "eu-east-1b"]
+    web_subnet_cidr_block = each.value["web_subnet_cidr_block"]
+    # public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+    enable_nat_gateway = true
+    enable_vpn_gateway = true
+
+    tags = var.tags
+    }
 } 
 
