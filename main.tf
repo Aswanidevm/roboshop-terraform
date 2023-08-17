846d2960963432e44f80bc4e7795c4c5da41fd70
Aswanidevm <maswanidev@gmail.com>
2023-08-17 08:22:32 +0530

    ad

--- main.tf
+++ main.tf
@@ -18,12 +18,12 @@ module "vpc" {
     
     env = var.env
 
-    azs             = ["eu-east-1a", "eu-east-1b"]
+    # azs             = ["eu-east-1a", "eu-east-1b"]
     web_subnet_cidr_block = each.value["web_subnet_cidr_block"]
     # public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-    enable_nat_gateway = true
-    enable_vpn_gateway = true
+    # enable_nat_gateway = true
+    # enable_vpn_gateway = true
 
     tags = var.tags
 }
