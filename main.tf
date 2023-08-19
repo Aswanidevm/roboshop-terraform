87340b40bca74c4308fc92e5b443cc53c1f57127
Aswanidevm <maswanidev@gmail.com>
2023-08-19 08:26:09 +0530

    sdfgb

--- main.tf
+++ main.tf
@@ -21,4 +21,13 @@ module "vpc" {
     tags = var.tags
 }
  
-
+ 
+module "app-server" {
+   
+    source = "git::https://github.com/Aswanidevm/tf-module-app.git"
+    
+    env = var.env
+    tags = var.tags
+    subnet_id = lookup(lookup(lookup(module.vpc,"subnet_ids",null), "main","subnet_ids",null), "app","subnet_ids"[0])
+    # module.vpc.subnet_ids.main.subnet_ids.app.subnet_ids[0]
+}
