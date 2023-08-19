731ca07f6289c4a4300b2193bfe37a83a98633b4
Aswanidevm <maswanidev@gmail.com>
2023-08-19 08:28:39 +0530

    aesd

--- main.tf
+++ main.tf
@@ -25,9 +25,9 @@ module "vpc" {
 module "app-server" {
    
     source = "git::https://github.com/Aswanidevm/tf-module-app.git"
-    
+    component= "test"
     env = var.env
     tags = var.tags
-    subnet_id = lookup(lookup(lookup(module.vpc,"subnet_ids",null), "main","subnet_ids",null), "app","subnet_ids"[0])
+    subnet_ids = lookup(lookup(lookup(module.vpc,"subnet_ids",null), "main","subnet_ids",null), "app","subnet_ids"[0])
     # module.vpc.subnet_ids.main.subnet_ids.app.subnet_ids[0]
 }
